2bb48d06bbe054ad0b07e2218034899474b71bb2
Alexey <alexeyj4@yandex.ru>
2024-05-09 22:31:07 +0300

    Update Step_motor_test.ino

--- ESP32-WROOM-32/Step_motor_test/Step_motor_test.ino
+++ ESP32-WROOM-32/Step_motor_test/Step_motor_test.ino
@@ -1,47 +1,32 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-stepper-motor-28byj-48-uln2003/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-  
-  Based on Stepper Motor Control - one revolution by Tom Igoe
-*/
-
-#include <Stepper.h>
-
-const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
-
-// ULN2003 Motor Driver Pins
+#include <GyverStepper.h>
+// ULN2003 Motor Driver Pins:
 #define IN1 19
 #define IN2 18
 #define IN3 5
 #define IN4 17
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
 
-// initialize the stepper library
-Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+GStepper<STEPPER4WIRE> stepper(stepsPerRevolution, IN4, IN2, IN3, IN1);
+// мотор с драйвером ULN2003 подключается по порядку пинов, но крайние нужно поменять местами
+// то есть у меня подключено D2-IN1, D3-IN2, D4-IN3, D5-IN4, но в программе поменял 5 и 2
 
 void setup() {
   Serial.setTimeout(100000);
-  Serial.begin(115200);   
+  Serial.begin(115200);
+  // режим следования к целевй позиции
+  stepper.setRunMode(FOLLOW_POS);
+  // установка макс. скорости в шагах/сек
+  stepper.setMaxSpeed(400);
+  // установка ускорения в шагах/сек/сек
+  stepper.setAcceleration(500);   
 }
 
 void loop() {
-  Serial.print("Введите скорость(1-15):");
-  String received_string=Serial.readStringUntil('\n');     
-  int speed=received_string.toInt();    
-  Serial.println(speed);
-  myStepper.setSpeed(speed);
-  Serial.println("ok");
-  
-  Serial.print("Введите угол:");
-  received_string=Serial.readStringUntil('\n');
-  Serial.println(received_string);
-  int steps=int(float(stepsPerRevolution)/360*received_string.toInt());       
-  //Serial.print("Шаги:");Serial.println(steps);//debug
-  myStepper.step(steps);  
-  Serial.println("ok");
+
+  // график положения
+  static uint32_t tmr2;
+  if (millis() - tmr2 > 20) {
+    tmr2 = millis();
+    Serial.println(stepper.getCurrent());
+  }
 }
